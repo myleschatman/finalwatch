af2522521811681a88ca622b190578d41011c6f2
Myles Chatman <myleschatman@gmail.com>
2018-03-07 21:44:39 -0800

    Add cursor keys movement for player

--- src/prefabs/player.js
+++ src/prefabs/player.js
@@ -5,8 +5,7 @@ export default class Player extends Phaser.Sprite {
 		this.game.physics.arcade.enableBody(this);
 		this.body.collideWorldBounds = true;
 
-		this.scale.x = 5;
-		this.scale.y = 5;
+		this.scale.x = this.scale.y = 5;
 		this.smoothed = false;
 		this.game.camera.follow(this);
 		
@@ -14,6 +13,23 @@ export default class Player extends Phaser.Sprite {
 	}
 	
 	update() {
+		this.body.velocity.x = 0;
+		this.body.velocity.y = 0;
 		
+		if (this.cursors.left.isDown) {
+			this.body.velocity =- 180;
+		} else if (this.cursors.right.isDown) {
+			this.body.velocity.x =+ 180;
+		} else {
+			this.body.velocity.x = 0;
+		}
+		
+		if (this.cursors.up.isDown) {
+			this.body.velocity.y =- 180;
+		} else if (this.cursors.down.isDown){
+			this.body.velocity.y =+ 180;
+		} else {
+			this.body.velocity.y = 0;
+		}
 	}
 }
