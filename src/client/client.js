81e6fd55d9951cc6b9061b02d702f9a656864950
Myles Chatman <myleschatman@gmail.com>
2018-03-22 14:13:18 -0700

    remove log data

--- src/client/client.js
+++ src/client/client.js
@@ -2,10 +2,9 @@ import io from 'socket.io-client';
 
 export default class Client {
 	constructor(game) {
-		this.socket = io.connect('http://138.68.251.157:8081');
+		this.socket = io.connect('http://localhost:8081');
 		
 		this.socket.on('newplayer', (data) => {
-			console.log(data);
 			game.addNewPlayer(data.id, data.x, data.y);
 		});
 	}
