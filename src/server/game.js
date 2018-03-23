f675d87346d7ead991406ea8355be3d58767ffff
Myles Chatman <myleschatman@gmail.com>
2018-03-23 02:01:57 -0700

    randomize player position and send players to client

--- src/server/game.js
+++ src/server/game.js
@@ -1,17 +1,32 @@
 export default class Game {
 	constructor(app) {
 		this.io = require('socket.io')(app);
-
 		this.lastPlayerId = 0;
-		this.io.on('connection', (socket) => {
+				this.io.on('connection', (socket) => {
 			socket.on('newplayer', () => {
 				socket.player = {
 					id: this.lastPlayerId++,
-					x: 0,
-					y: 0
+					x: this.randomInt(100, 400),
+					y: this.randomInt(100, 400)
 				};
-				socket.emit('newplayer', socket.player);
-			});
+				socket.emit('allplayers', this.getAllPlayers());
+				socket.broadcast.emit('newplayer', socket.player);
+			})
+		})
+	}
+
+	getAllPlayers() {
+		var players = []
+		var self = this
+		Object.keys(self.io.sockets.connected).forEach(function (socketID) {
+				var player = self.io.sockets.connected[socketID].player;
+				if (player) players.push(player);
 		});
+		console.log(players);
+		return players;
+	}
+
+	randomInt(low, high) {
+		return Math.floor(Math.random() * (high - low) + low);
 	}
 }
