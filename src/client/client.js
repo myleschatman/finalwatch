f44ad2b1f58cc7857ddaa1e362459c08f5cd5ad4
Myles Chatman <myleschatman@gmail.com>
2018-03-23 02:01:09 -0700

    load all connected players

--- src/client/client.js
+++ src/client/client.js
@@ -7,6 +7,12 @@ export default class Client {
 		this.socket.on('newplayer', (data) => {
 			game.addNewPlayer(data.id, data.x, data.y);
 		});
+
+		this.socket.on('allplayers', (data) => {
+			for (let i = 0; i < data.length; i++) {
+				game.addNewPlayer(data[i].id, data[i].x, data[i].y);
+			}
+		});
 	}
 
 	getNewPlayer() {
